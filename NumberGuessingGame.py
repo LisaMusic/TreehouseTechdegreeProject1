d1ca78d2500cdec313db50709f606ed8957ef40d
LisaMusic <147201402+LisaMusic@users.noreply.github.com>
2023-10-27 18:19:13 +0200

    update

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -14,7 +14,11 @@ def start_game ():
             try:
                 player_guess = input ("Enter your guess:   ")
                 player_guess = int (player_guess)
+                if player_guess < 1 or player_guess >100:
+                    raise ValueError
+                
                 guess_attempts +=1
+                
                 if player_guess > random_number:
                     print ("It's lower.")
                 elif player_guess < random_number:
@@ -24,18 +28,24 @@ def start_game ():
                     attempts.append(guess_attempts)
                     break
             except ValueError:
-                print("Please enter a number between 1 and 100.")
+                print("Invalid input. Please enter a number between 1 and 100.")
 
-        print ("You needed {} attempts.".format (guess_attempts))
-        print ("Mean: {}".format (mean(attempts)))
-        print ("Median: {}".format (median(attempts)))
-        print ("Mode: {}".format (mode(attempts)))
+        print (
+            f"You needed {guess_attempts} attempts.\n"
+            f"Mean: {mean(attempts)}\n"
+            f"Median: {median(attempts)}\n"
+            f"Mode: {mode(attempts)}"
+        )
 
-        play_again = input ("Do you want to play again? Y/N")
-        if play_again.lower() == "y":
-            random_number = random.randint (1,100)
-            print ("Here we go. Let's see if you can beat your score:{}".format(min(attempts)))
-        else:
-            print ("OK. Bye!")
+        while True:
+            play_again = input ("Do you want to play again? Y/N")
+            if play_again.lower() == "y":
+                print (f"Here we go. Let's see if you can beat your score:{min(attempts)}")
+                break
+            elif play_again.lower()=="n":
+                print ("OK. Bye!")
+                return
+            else:
+                print ("Please enter 'Y' or 'N'.")
 
 start_game()
