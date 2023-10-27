06cf86c80f14a7fda9b1cab5f9e786dcbdfb3b91
LisaMusic <147201402+LisaMusic@users.noreply.github.com>
2023-10-27 13:00:44 +0200

    update

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -9,34 +9,32 @@ def start_game ():
     while True:
         print ("Welcome to the number guessing game! Please choose a number between 1 and 100.")
         random_number = random.randint (1,100)
-        game_attempts = 0
+        guess_attempts = 0
         while True:
             try:
                 player_guess = input ("Enter your guess:   ")
                 player_guess = int (player_guess)
-                game_attempts +=1
+                guess_attempts +=1
                 if player_guess > random_number:
                     print ("It's lower.")
                 elif player_guess < random_number:
                     print ("It's higher.")
                 else:
-                    print ("Congrats, you guessed it!")
-                    attempts.append(game_attempts)
+                    print ("Congrats, you got it!")
+                    attempts.append(guess_attempts)
                     break
             except ValueError:
                 print("Please enter a number.")
 
-#   5. Display the following data to the player
-#     a. Display attempts it took them to get the correct number in this game
-        print ("You needed {} attempts.".format (game_attempts))
-#     b. The mean of the saved attempts list
-#     c. The median of the saved attempts list
-#     d. The mode of the saved attempts list
+        print ("You needed {} attempts.".format (guess_attempts))
+        print ("Mean: {}".format (mean(attempts)))
+        print ("Median: {}".format (median(attempts)))
+        print ("Mode: {}".format (mode(attempts)))
 
         play_again = input ("Do you want to play again? Y/N")
         if play_again.lower() == "y":
             random_number = random.randint (1,100)
-            print ("Here we go. Let's see if you can beat your score")
+            print ("Here we go. Let's see if you can beat your score:{}".format(min(attempts)))
         else:
             print ("OK. Bye!")
 start_game()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # TreehouseTechdegreeProject1
- My first Techdegree project
+
+My first Techdegree project is a number guessing game.
+In the game, the program selects a random number between 1 and 100, and the user needs to guess the correct number.
+It will also provide you with your game statistics.
+
+Enjoy!
+
